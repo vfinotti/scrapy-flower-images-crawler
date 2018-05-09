3076f5a70c55844e610344e590d24c1035014c8f
Vitor Finotti <vfinotti@gmail.com>
2018-05-09 20:46:15 -0300

    Adding code that downloads the images

--- sugarcanespyder/sugarcanespyder/spiders/sugarcanespider.py
+++ sugarcanespyder/sugarcanespyder/spiders/sugarcanespider.py
@@ -1,4 +1,4 @@
-from sugarcanespyder.items import SugarcaneSpider
+from sugarcanespyder.items import SugarcaneItem
 import datetime
 import scrapy
 import time
@@ -27,21 +27,14 @@ class SugarcaneSpider(scrapy.Spider):
         current_page = int(response.xpath("//*[contains(@class, 'pagination-container id')]//*[contains(@class, 'form-control')]/@value").extract_first())
         print(current_page)
 
-        # # picking all 'tr' nodes, no matter where they are, if the subnode
-        # # 'td/table/tbody/tr/td/b' contains the text 'Plant:'
-        # url = response.xpath('//tr[./td/table/tbody/tr/td/b[contains(., "Plant:")]]')
+        # picking all nodes if the class is 'img-wrap', no matter where they
+        # are, and then selecting the 'src' attriute (it's where the image URL
+        # is) from 'img' nodes
+        images = response.xpath("//*[contains(@class, 'img-wrap')]//img/@src")
+        imagesURLs = images.extract()
 
-        # # download if all elements (20) were downloaded or if this is the last
-        # # page, which doesn't always contain 20 elements
-        # if ((len(url) == 20) or (current_page == last_page)):
-        #     for leavesset in url:
-        #         imageURL = 'https://garden.org' + leavesset.css('img ::attr(src)').extract_first()
-        #         # Replace string to download the orignal photo instead of the 250x250 px thumbnails (the only difference on their link is this part!)
-        #         imageURL = imageURL.replace("-250.jpg", ".jpg", 1)
-        #         leaves_name = extract_whole_text(leavesset.css('a ::text'))
-
-        #         yield GardenFlower(leaves_name=leaves_name, page=current_page, file_urls=[imageURL])
-
+        for imageURL in imagesURLs:
+            yield SugarcaneItem(page=current_page, file_urls=[imageURL])
         
         # # reload page if there are less then 20 pictures and it is not the last
         # # page
